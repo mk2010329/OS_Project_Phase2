1d71d8f129c7d467c1c135919931146f066e03bc
basil-bm2108258 <shukraan43@gmail.com>
2024-04-22 18:54:51 +0300

    made classes Leaderboard and Player

---
+++ OS_Project/src/Player.java
@@ -0,0 +1,56 @@
+
+public class Player {
+
+	private String nickname;
+	private int numberOfWins;
+	private String ticket;
+	private int gamePoints;
+	
+	public Player(String nickname, int numberOfWins, String ticket, int gamePoints) {
+		
+		this.nickname = nickname;
+		this.numberOfWins = numberOfWins;
+		this.ticket = ticket;
+		this.gamePoints = gamePoints;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getNumberOfWins() {
+		return numberOfWins;
+	}
+
+	public void setNumberOfWins(int numberOfWins) {
+		this.numberOfWins = numberOfWins;
+	}
+
+	public String getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(String ticket) {
+		this.ticket = ticket;
+	}
+
+	public int getGamePoints() {
+		return gamePoints;
+	}
+
+	public void setGamePoints(int gamePoints) {
+		this.gamePoints = gamePoints;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [nickname=" + nickname + ", numberOfWins=" + numberOfWins + ", ticket=" + ticket
+				+ ", gamePoints=" + gamePoints + "]";
+	}
+
+	
+}
