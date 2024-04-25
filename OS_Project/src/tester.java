e769638fb68ffbca34f73eaef347059637980f96
Mahodidothsp <123658483+Mahodidothsp@users.noreply.github.com>
2024-04-25 15:29:56 +0300

    conflict commit

--- OS_Project/src/tester.java
+++ OS_Project/src/tester.java
@@ -1,20 +1,16 @@
 
-public class tester {
+public class Tester {
 
 	public static void main(String[] args) {
 		
-		Player p1 = new Player("say", 5, "s", 0);
-		Player p2 = new Player("jay", 2, "j", 0);
-		Player p3 = new Player("fay", 3, "f", 0);
-		
-		Leaderboard lb = new Leaderboard();
-		
-		lb.putPlayer(p1);
-		lb.putPlayer(p2);
-		lb.putPlayer(p3);
+		Game game = new Game();
+		game.listOfCurrentGuesses.add(1);
+		game.listOfCurrentGuesses.add(1);
+		game.listOfCurrentGuesses.add(3);
+		game.listOfCurrentGuesses.add(2);
 		
 		
-
+		System.out.println(game.getAverage());
 	}
 
 }
