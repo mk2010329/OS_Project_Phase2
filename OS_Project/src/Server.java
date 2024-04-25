3ba89be7508d5d736c9b85a552e3383b2747f8d1
Mahodidothsp <123658483+Mahodidothsp@users.noreply.github.com>
2024-04-26 02:01:00 +0300

    updates

--- OS_Project/src/Server.java
+++ OS_Project/src/Server.java
@@ -3,13 +3,20 @@ import java.io.*;
 import java.util.*;
 
 public class Server {
+	
+	private static ArrayList<Game> listOfGames; //array list to store games
+	
 	public static void main(String args[]) {
 		try {
 			// Bind to service port, so clients access to daytime service
+			@SuppressWarnings("resource")
 			ServerSocket server = new ServerSocket(13337);
 			System.out.println("Server waiting for client on port " +
 					server.getLocalPort());
 			System.out.println("Day Time Service Started");
+			
+			setListOfGames(new ArrayList<>()); //Initializing list of games array list
+			
 			for(;;) {
 				// Get the next TCP Client
 				Socket nextClient = server.accept();
@@ -29,6 +36,14 @@ public class Server {
 		} catch(IOException ioe){
 			System.out.println("Error" + ioe);
 		}
+	}
+
+	public static ArrayList<Game> getListOfGames() {
+		return listOfGames;
+	}
+
+	public static void setListOfGames(ArrayList<Game> listOfGames) {
+		Server.listOfGames = listOfGames;
 	}
 	
 	 
